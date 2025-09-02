16c7107408f94eace49c8e431b04a64a9290784c
PMDToufiqKhan <toufiqkhan809@gmail.com>
2025-09-02 07:53:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import nltk
 from nltk.stem.porter import PorterStemmer
 import nltk
 nltk.download('punkt_tab')
+nltk.download('stopwords')
 
 ps = PorterStemmer()
 def transform_text(text):
@@ -55,4 +56,5 @@ if st.button('Predict'):
         st.header("Spam")
     else:
         st.header("Not Spam")
+
 
